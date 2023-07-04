f3937129730997d3a04fe8739b5b35fae38af2ce
Danilo Oliveira <cardoso.1007@hotmail.com>
2023-07-04 13:46:09 -0300

    adicionado assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.combine([
+    "resources/admin/vendor/fontawesome-free/css/all.min.css",
+    "resources/admin/css/sb-admin-2.min.css"
+], "public/css/admin.css");
+
+mix.combine([
+    "resources/admin/vendor/jquery/jquery.min.js",
+    "resources/admin/vendor/bootstrap/js/bootstrap.bundle.min.js",
+    "resources/admin/vendor/jquery-easing/jquery.easing.min.js",
+    "resources/admin/js/sb-admin-2.min.js",
+    "resources/admin/vendor/chart.js/Chart.min.js",
+    "resources/admin/js/demo/chart-area-demo.js",
+    "resources/admin/js/demo/chart-pie-demo.js",
+], "public/js/admin.js");
