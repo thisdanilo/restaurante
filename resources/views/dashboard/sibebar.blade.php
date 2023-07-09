630e4b7365f8a187eef2c5ec58afe8a7ce096589
Danilo Oliveira <doliveira100720@gmail.com>
2023-07-09 14:24:12 -0300

    adicionado regra sidebar

--- resources/views/dashboard/sibebar.blade.php
+++ resources/views/dashboard/sibebar.blade.php
@@ -1,33 +1,36 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('tenant.index') }}">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-utensils"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Restaurantes</div>
     </a>
 
-
     <hr class="sidebar-divider my-0">
 
     <li class="nav-item">
-        <a class="nav-link" href="{{ url('/') }}">
+        <a class="nav-link" href="{{ route('tenant.index') }}">
             <i class="fas fa-home"></i>
             <span>Dashboard</span></a>
     </li>
 
-    <li class="nav-item {{ Ekko::areActiveRoutes(['tenant*'], 'active') }}">
-        <a class="nav-link" href="{{ route('tenant.index') }}">
-            <i class="fas fa-store-alt"></i>
-            <span>Restaurantes</span>
-        </a>
-    </li>
+    @can('tenant_show')
+        <li class="nav-item {{ Ekko::areActiveRoutes(['tenant*'], 'active') }}">
+            <a class="nav-link" href="{{ route('tenant.index') }}">
+                <i class="fas fa-store-alt"></i>
+                <span>Restaurantes</span>
+            </a>
+        </li>
+    @endcan
 
-    <li class="nav-item {{ Ekko::areActiveRoutes(['role*'], 'active') }}">
-        <a class="nav-link" href="{{ route('role.index') }}">
-            <i class="fas fa-lock"></i>
-            <span>Funções</span>
-        </a>
-    </li>
+    @can('role_show')
+        <li class="nav-item {{ Ekko::areActiveRoutes(['role*'], 'active') }}">
+            <a class="nav-link" href="{{ route('role.index') }}">
+                <i class="fas fa-lock"></i>
+                <span>Funções</span>
+            </a>
+        </li>
+    @endcan
 
 </ul>
