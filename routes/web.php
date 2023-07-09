926f9392cfe5c82bb8763234b5a93c760d13e499
Danilo Oliveira <doliveira100720@gmail.com>
2023-07-09 09:55:13 -0300

    ajuste rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\TenantController;
 
 /*
@@ -28,17 +29,40 @@ Route::group([
 
     Route::post('/datatable', 'datatable')->middleware('can:tenant_show')->name('datatable');
 
-    Route::get('{tenant}/ver', 'show')->middleware('can:tenant_show')->name('show');
+    Route::get('{id}/ver', 'show')->middleware('can:tenant_show')->name('show');
 
     Route::get('/cadastrar', 'create')->middleware('can:tenant_create')->name('create');
 
     Route::post('/', 'store')->middleware('can:tenant_create')->name('store');
 
-    Route::get('{tenant}/editar', 'edit')->middleware('can:tenant_edit')->name('edit');
+    Route::get('{id}/editar', 'edit')->middleware('can:tenant_edit')->name('edit');
 
-    Route::put('{tenant}/editar', 'update')->middleware('can:tenant_edit')->name('update');
+    Route::put('{id}/editar', 'update')->middleware('can:tenant_edit')->name('update');
 
-    Route::delete('{tenant}/excluir', 'delete')->middleware('can:tenant_delete')->name('delete');
+    Route::delete('{id}/excluir', 'delete')->middleware('can:tenant_delete')->name('delete');
+});
+
+Route::group([
+    'prefix' => 'dashboard/funcao',
+    'as' => 'role.',
+    'controller' => RoleController::class,
+    'middleware' => 'auth'
+], function () {
+    Route::get('/', 'index')->middleware('can:role_show')->name('index');
+
+    Route::post('/datatable', 'datatable')->middleware('can:role_show')->name('datatable');
+
+    Route::get('{id}/ver', 'show')->middleware('can:role_show')->name('show');
+
+    Route::get('/cadastrar', 'create')->middleware('can:role_create')->name('create');
+
+    Route::post('/', 'store')->middleware('can:role_create')->name('store');
+
+    Route::get('{id}/editar', 'edit')->middleware('can:role_edit')->name('edit');
+
+    Route::put('{id}/editar', 'update')->middleware('can:role_edit')->name('update');
+
+    Route::delete('{id}/excluir', 'delete')->middleware('can:role_delete')->name('delete');
 });
 
 require __DIR__ . '/auth.php';
